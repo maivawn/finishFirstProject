99a424c8ca6d11f765fcf87ef70181dde9b98a5c
Mai Van Manh <maimanh1422002@gmail.com>
2024-03-06 15:17:37 +0700

    commit menulevel

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -9,7 +9,17 @@ public class Finish : MonoBehaviour
     {
         if (other.gameObject.name == ("Player"))
         {
+            UnlockNewlevel();
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+    void UnlockNewlevel()
+    {
+        if(SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex",SceneManager.GetActiveScene().buildIndex +1 );
+            PlayerPrefs.SetInt("unlockedLevel", PlayerPrefs.GetInt("unlockedLevel",1) + 1 );
+            PlayerPrefs.Save();
         }
     }
 }
